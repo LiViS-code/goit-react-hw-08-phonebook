ea2c70d269a5baf08e1866ec25b3620c80465fe8
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-02-23 16:41:54 +0200

    added page Home

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,11 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
     <>
       <h2>Welcome</h2>
       <p>Create your phone book of contacts</p>
-      <p><NavLink to={"/register"}>Register</NavLink> (for new users) or <NavLink to={"/login"}>login</NavLink> to get access to your contacts.</p>
+      <p><Link to={"/register"}>Register</Link> (for new users) or <Link to={"/login"}>login</Link> to get access to your contacts.</p>
     </>
   )
 };

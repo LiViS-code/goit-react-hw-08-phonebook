9e8590fb99f332b453ddacca5b6fec58ad29d102
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-03-07 09:37:10 +0200

    added func login

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,15 @@
+import PropTypes from "prop-types";
 import LoginForm from "components/Forms/LoginForm/LoginForm";
 
-export default function LoginPage() {
+export default function LoginPage({onLoginUsr}) {
   return (
     <>
       <h2>Authorization</h2>
-      <LoginForm />
+      <LoginForm onLoginUsr={onLoginUsr}/>
     </>
   )
 }    
+
+LoginPage.propTypes = {
+  onLoginUsr: PropTypes.func.isRequired,
+}
--- src/components/Forms/LoginForm/LoginForm.jsx
+++ src/components/Forms/LoginForm/LoginForm.jsx
@@ -1,15 +1,44 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
 import { Form, Input, Label, Button } from "../Forms.styled";
 
-export default function LoginForm() {
+export default function LoginForm({onLoginUsr}) {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case "login":
+        setLogin(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLoginUsr(login, password);
+  }
+
   return (
-    <Form>
-      <Label htmlFor="login">Login</Label>
-      <Input type="text" name="Login" placeholder='input login' />
+    <Form onSubmit={handleSubmit}>
+      <Label htmlFor="login">Name</Label>
+      <Input type="text" name="login" value={login} placeholder="Enter login" required onChange={handleInput}/>
       
       <Label htmlFor="password">Password</Label>
-      <Input type="text" name="password" placeholder="input password" />
+      <Input type="password" name="password" value={password} placeholder="Enter password" required onChange={handleInput}/>
 
-      <Button type="button">Log In</Button>
+      <Button type="submit">Log In</Button>
     </Form>
   )
 };
+
+LoginForm.propTypes = {
+  onLoginUsr: PropTypes.func.isRequired,
+}
